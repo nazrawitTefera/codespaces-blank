bb9bf52322ebf7695e8a4c09dfece61bfa19f928
naz <nazikefyalew@gmail.com>
2025-09-03 17:53:11 -0400

    Update Easter.java

--- Easter.java
+++ Easter.java
@@ -1,7 +1,21 @@
+//Nazrawit Tefera
+//01.03.08- Practice With Java Variables
 public class Easter{
     public static void main(String[] args) {
-        //code goes here
-        //some more code
+        int y= 2003;
+        int a = y % 19;
+        int b= y/100;
+        int c= y % 100;
+        int f=(b+8)/25;
+        int g = (b - f + 1) / 3;
+        nt h = (19*a + b - d - g + 15) % 30;
+        int i = c / 4;
+        int k = c % 4;
+        int r = (32 + 2*e + 2*i - h - k) % 7;
+        int m = (a + 11*h + 22*r) / 451;
+        int n=(h+r-7*m+114)/31;
+        int p=(h+r-7*m+114)%31;
+        
     }
 
 }
